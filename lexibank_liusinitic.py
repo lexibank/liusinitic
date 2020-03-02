1b40413dbf275a09e0dd19b94ba66ee6d6dbe3e9
MacyL <wumeishin@gmail.com>
2020-03-02 16:09:55 +0100

    update the python script

--- lexibank_liusinitic.py
+++ lexibank_liusinitic.py
@@ -3,20 +3,20 @@ from collections import OrderedDict, defaultdict
 
 import attr
 from clldutils.misc import slug
-from clldutils.path import Path
-from clldutils.text import split_text, strip_brackets
-from pylexibank.dataset import NonSplittingDataset as BaseDataset
-from pylexibank.dataset import Concept, Language
+from pathlib import Path
+from pylexibank.forms import FormSpec
+from pylexibank import Dataset as BaseDataset
+from pylexibank import Concept, Language
+from pylexibank.util import progressbar
 
 from lingpy import *
-from tqdm import tqdm
 
 @attr.s
-class HConcept(Concept):
+class CustomConcept(Concept):
     Chinese_Gloss = attr.ib(default=None)
 
 @attr.s
-class HLanguage(Language):
+class CustomLanguage(Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     ChineseName = attr.ib(default=None)
@@ -31,46 +31,50 @@ class HLanguage(Language):
 class Dataset(BaseDataset):
     id = 'liusinitic'
     dir = Path(__file__).parent
-    concept_class = HConcept
-    language_class = HLanguage
+    concept_class = CustomConcept
+    language_class = CustomLanguage
 
-    def cmd_download(self, **kw):
-        pass
-
-    def cmd_install(self, **kw):
-
-        wl = Wordlist(self.dir.joinpath('raw', 'words-new.tsv').as_posix())
-        langs = {} # need for checking later
+    def cmd_makecldf(self, args):
+        # add source
+        args.writer.add_sources()
+        # read in data
+        ds = self.raw_dir / "words-new.tsv"
+        wl = Wordlist(ds.as_posix())
+        # add languages
+        languages = args.writer.add_languages(lookup_factory="Name")
+        # add concepts
         concepts = {}
-
-        with self.cldf as ds:
-
-            for concept in self.conceptlist.concepts.values():
-                ds.add_concept(
-                        ID=concept.id,
-                        Name=concept.english,
-                        Chinese_Gloss=concept.attributes['chinese'],
-                        Concepticon_ID=concept.concepticon_id,
-                        Concepticon_Gloss=concept.concepticon_gloss
-                        )
-                concepts[concept.english] = concept.id
-            concepts['heart [compound]'] = 'Liu-2007-201-158'
-            concepts['river_2'] = 'Liu-2007-201-50'
-            concepts['river'] = 'Liu-2007-201-49'
-            ds.add_languages()
-            langs = {k['Name']: k['ID'] for k in self.languages}
-            ds.add_sources(*self.raw.read_bib())
-            bads = []
-            for idx in tqdm(wl, desc='cldfify'):
-                
-                ds.add_lexemes(
-                   Language_ID=langs[wl[idx, 'doculect']],
-                   Parameter_ID=concepts[wl[idx, 'concept']], 
-                   Value=wl[idx, 'value'],
-                   Segments=wl[idx, 'tokens'],
-                   Source=['Liu2007']
-                   )
-
-
-
-
+        for concept in self.conceptlists[0].concepts.values():
+            args.writer.add_concept(
+                ID=concept.id,
+                Name=concept.english,
+                Chinese_Gloss=concept.attributes["chinese"],
+                Concepticon_ID=concept.concepticon_id,
+                Concepticon_Gloss=concept.concepticon_gloss)
+            concepts[concept.english] = concept.id
+        # add the concepts which appear in the word list but do not appear in the concepticon list.
+        concepts['heart [compound]'] = 'Liu-2007-201-158'
+        concepts['river_2'] = 'Liu-2007-201-50'
+        concepts['river'] = 'Liu-2007-201-49'
+         # add forms
+        for idx in progressbar(wl, desc="cldfify the data"):
+            cogid = idx
+            if wl[idx, "concept"]:
+                args.writer.add_form_with_segments(
+                    Language_ID=languages[wl[idx, "doculect"]],
+                    Parameter_ID=concepts[wl[idx, "concept"]],
+                    Value=wl[idx, "value"],
+                    Form=wl[idx,"value"],
+                    Segments=wl[idx,"segments"],
+                    Source=['Liu2007']
+                )
+        #     bads = []
+        #     for idx in tqdm(wl, desc='cldfify'):
+        #
+        #         ds.add_lexemes(
+        #            Language_ID=langs[wl[idx, 'doculect']],
+        #            Parameter_ID=concepts[wl[idx, 'concept']],
+        #            Value=wl[idx, 'value'],
+        #            Segments=wl[idx, 'tokens'],
+        #            Source=['Liu2007']
+        #            )
